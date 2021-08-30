e541591a4a9ca5d7fa3c49522eb25a6ae4edafba
greenhandatsjtu <sunhengke@sjtu.edu.cn>
2021-08-30 19:32:42 +0800

    finish lab2-wrap

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -5,18 +5,12 @@
 // For Lab 2, please replace with a real implementation that passes the
 // automated checks run by `make check_lab2`.
 
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
-
 using namespace std;
 
 //! Transform an "absolute" 64-bit sequence number (zero-indexed) into a WrappingInt32
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
-WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
-}
+WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) { return isn + static_cast<uint32_t>(n); }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
 //! \param n The relative sequence number
@@ -29,6 +23,13 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    WrappingInt32 w = wrap(checkpoint, isn);
+    uint64_t a = static_cast<int64_t>(n - w) + checkpoint;
+    uint64_t b = a + (1ull << 32);
+    uint64_t c = a - (1ull << 32);
+    uint64_t distance_a = a > checkpoint ? a - checkpoint : checkpoint - a;
+    uint64_t distance_b = b > checkpoint ? b - checkpoint : checkpoint - b;
+    uint64_t distance_c = c > checkpoint ? c - checkpoint : checkpoint - c;
+    return distance_a < distance_b ? (distance_a < distance_c ? a : (distance_c < distance_b ? c : 0))
+                                   : (distance_b < distance_c ? b : (distance_c < distance_a ? c : 0));
 }
